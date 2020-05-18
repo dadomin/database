817b074d36223e61be515efb158de50e8454459f
dadomin <brlight_dm@naver.com>
2020-05-19 06:09:00 +0900

    20200518 월요일  과제 완료

---
+++ 온라인 과제 sql/p.47.sql
@@ -0,0 +1,4 @@
+SELECT * FROM employees;
+SELECT FIRST_NAME AS "이름", phone_number AS "전화번호", job_id AS "소속" FROM employees;
+SELECT DEPARTMENT_ID AS "부서_id", DEPARTMENT_NAME AS "부서명", MANAGER_ID AS "mgr_id" FROM DEPARTMENTS;
+
